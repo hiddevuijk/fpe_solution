d037e2a6027fe29ae3689323cbc379435634f381
hiddevuijk <hdvuijk@gmail.com>
2020-01-22 18:59:03 +0100

    start system

---
+++ system.h
@@ -0,0 +1,17 @@
+#ifndef GAURD_SYSTEM_H
+#define GAURD_SYSTEM_H
+
+#include <vector>
+
+class System {
+
+
+    int Nx,Ny;
+    std::vector<std::vector<double> > P;
+    std::vector<std::vector<double> > J;
+
+
+};
+
+
+#endif
